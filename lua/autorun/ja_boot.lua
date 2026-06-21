a7a9def89d37e391fecf561680cbfd25517a6467
Lordian Ayles <gkai70263@gmail.com>
2026-06-20 23:08:05 -0400

    reorganizing(): testing new structures like specific entities for kills, generics, etc.

--- lua/autorun/ja_boot.lua
+++ lua/autorun/ja_boot.lua
@@ -4,28 +4,28 @@ hook.Add("Initialize", "JaVox Initialize Function", function()
     JaVox:registerModule("test", {
         displayName = "Test Module",
         actions = {
-            ["reload"] = {
-                priority = AudioPriority.PLAY_ONCE_WITHOUT_DEFERRAL,
+            ["weaponry"] = {
+                ["reload"] = {
+                    priority = AudioPriority.PLAY_ONCE_WITHOUT_DEFERRAL,
 
-                audioFiles = {
-                    "javox/reloading1.wav",
-                    "javox/reloading2.wav",
-                    "javox/reloading3.wav",
-                    "javox/reloading4.wav",
-                    "javox/reloading5.wav",
-                    "javox/reloading6.wav",
-                    "javox/reloading7.wav",
-                    "javox/reloading10.wav",
-                    "javox/reloading12.wav",
+                    audioFiles = {
+                        "javox/reloading1.wav",
+                        "javox/reloading2.wav",
+                        "javox/reloading3.wav",
+                        "javox/reloading4.wav",
+                        "javox/reloading5.wav",
+                        "javox/reloading6.wav",
+                        "javox/reloading7.wav",
+                        "javox/reloading10.wav",
+                        "javox/reloading12.wav",
+                    },
+
+                    delay = {
+                        min = 0.2,
+                        max = 0.7,
+                    }
                 },
 
-                delay = {
-                    min = 0.2,
-                    max = 0.7,
-                }
-            },
-
-            ["weaponry"] = {
                 ["grenadeOut"] = {
                     priority = AudioPriority.PLAY_ONCE_WITHOUT_DEFERRAL,
                     audioFiles = {
@@ -41,6 +41,40 @@ hook.Add("Initialize", "JaVox Initialize Function", function()
                         max = 1,
                     }
                 },
+            },
+
+            ["ents"] = {
+                ["entKillGeneric"] = {
+                    priority = AudioPriority.PLAY_ONCE_WITHOUT_DEFERRAL,
+                    delay = {
+                        min = 0.3,
+                        max = 1,
+                    },
+                    audioFiles = {
+                        "javox/target1.wav",
+                        "javox/target2.wav",
+                        "javox/target3.wav",
+                        "javox/target4.wav",
+                        "javox/target5.wav",
+                        "javox/target6.wav",
+                        "javox/target7.wav",
+                        "javox/target8.wav",
+                        "javox/target9.wav",
+                    }
+                },
+
+                ["specific"] = {
+                    ["npc_metropolice"] = {
+                        priority = AudioPriority.PLAY_ONCE_WITHOUT_DEFERRAL,
+                        delay = {
+                            min = 0.2,
+                            max = 0.7
+                        },
+                        audioFiles = {
+                            "javox/fa.wav"
+                        }
+                    }
+                }
             }
         },
         callouts = {
