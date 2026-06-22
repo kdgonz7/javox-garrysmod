b67028e54cb7a97dab96b8ce5618a7f942fa3ef3
Lordian Ayles <gkai70263@gmail.com>
2026-06-22 09:06:52 -0400

    Add new fonts for visual hierarchy

--- lua/autorun/client/ja_fonts.lua
+++ lua/autorun/client/ja_fonts.lua
@@ -1,6 +1,20 @@
 surface.CreateFont("JaVoxRegular", {
     font = "Roboto",
-    size = 15,
+    size = 16,
+    extended = true,
+    antialias = true,
+})
+
+surface.CreateFont("JaVoxSmaller", {
+    font = "Roboto",
+    size = 14,
+    extended = true,
+    antialias = true,
+})
+
+surface.CreateFont("JaVoxSmallest", {
+    font = "Roboto",
+    size = 12,
     extended = true,
     antialias = true,
 })
