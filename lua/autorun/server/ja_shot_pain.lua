2e12635017018cc404e89bdaa76633c9cf7b2268
Lordian Ayles <gkai70263@gmail.com>
2026-06-22 17:10:26 -0400

    add docs for shot pain

---
+++ lua/autorun/server/ja_shot_pain.lua
@@ -0,0 +1,4 @@
+hook.Add("EntityTakeDamage", "JaVOX Shot Pain Module", function(target, dmg)
+    -- TODO: check both entities, if recipient is player and they have preset, call their javox 'self.inflictedDamage.shot.rarm, larm, etc.', or specific ones
+    -- TODO: always have generic editions.
+end)
