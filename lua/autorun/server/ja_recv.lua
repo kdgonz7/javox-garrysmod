63a6a3f502fbfab814e0c0ccb387dd6b95620766
Lordian Ayles <gkai70263@gmail.com>
2026-06-22 09:06:58 -0400

    Document server API

--- lua/autorun/server/ja_recv.lua
+++ lua/autorun/server/ja_recv.lua
@@ -4,12 +4,14 @@
 util.AddNetworkString("JaVox_EmitCallout")
 util.AddNetworkString("JaVox_ChangePlayerPreset")
 
-
+-- /preset/[id]
 net.Receive("JaVox_ChangePlayerPreset", function(len, ply)
     local id = net.ReadString()
     ply:SetNWString(JAVOX_PRESET, id)
     JaVox.State:clearPlayerQueue(ply:EntIndex())
 end)
+
+-- /callout/emit/[name]
 net.Receive("JaVox_EmitCallout", function(len, ply)
     local calloutName = net.ReadString()
     JaVox.Director:emitCalloutFromPlayer(ply, calloutName)
