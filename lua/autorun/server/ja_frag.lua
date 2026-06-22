ba9671201075309c1a70e191f2a9ac91dc32bea6
Lordian Ayles <gkai70263@gmail.com>
2026-06-22 10:35:25 -0400

    note: this is how i find out ARC9 modifies gun ammo types

--- lua/autorun/server/ja_frag.lua
+++ lua/autorun/server/ja_frag.lua
@@ -1,14 +1,16 @@
 print("Entity frag module loaded!")
-
+-- TODO: configs for all builtin modules.
+-- TODO: we love the player.
 local GRENADES = {
     10, -- classic HL2
-    45  -- arc9 frag
+    45, -- arc9 frag
+    46  -- arc9 frag
 }
 
 hook.Add("KeyPress", "JaVOX Frag Out", function(ply, key)
     if key == IN_ATTACK then
         local weap = ply:GetActiveWeapon()
-        if ! IsValid(weap) then return end
+        if ! IsValid(weap) then return print("Not valid") end
         if table.HasValue(GRENADES, weap:GetPrimaryAmmoType()) then
             JaVox.Director:emitActionFromPlayer(ply, "weaponry.grenadeOut")
         end
