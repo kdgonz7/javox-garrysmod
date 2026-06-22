efe9dcf501781d13fc31541aa74d8fb010e4019f
Lordian Ayles <gkai70263@gmail.com>
2026-06-22 17:04:40 -0400

    todo: go back to jam feature

--- lua/autorun/server/ja_reload.lua
+++ lua/autorun/server/ja_reload.lua
@@ -7,6 +7,11 @@ hook.Add("KeyPress", "JaVox Key Press Test", function(ply, key)
             return
         end
 
+        -- TODO: we can build a jam module using this.
+        -- TODO: simple, we hook a loop that checks all like weapons to see if they are GetJammed (ARC9) and ACTUALLY ARE jammed.
+        -- TODO: if they are, play that sound. Maybe do it in attack beacuse they click when it jams.
+        print(activeWeapon.GetJammed)
+
         -- TODO: check for ammo types.
         JaVox.Director:emitActionFromPlayer(ply, "weaponry.reload")
     end
