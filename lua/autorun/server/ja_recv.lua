0c76528c9f1470a9ad1209bc167ec30de22e7542
Lordian Ayles <gkai70263@gmail.com>
2026-06-21 23:32:44 -0400

    Change receiver purpose and add change preset call

--- lua/autorun/server/ja_recv.lua
+++ lua/autorun/server/ja_recv.lua
@@ -1,9 +1,15 @@
---- JaVox's basic networking listener.
----     Does nothing but ensure callouts from client can pass and call.
+--- JaVox's listener.
 --- Client -> Callout Func -> Server calls from player
 
 util.AddNetworkString("JaVox_EmitCallout")
+util.AddNetworkString("JaVox_ChangePlayerPreset")
 
+
+net.Receive("JaVox_ChangePlayerPreset", function(len, ply)
+    local id = net.ReadString()
+    ply:SetNWString(JAVOX_PRESET, id)
+    JaVox.State:clearPlayerQueue(ply:EntIndex())
+end)
 net.Receive("JaVox_EmitCallout", function(len, ply)
     local calloutName = net.ReadString()
     JaVox.Director:emitCalloutFromPlayer(ply, calloutName)
