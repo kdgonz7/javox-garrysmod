f0c4152716e0607003f415da92699f32c0aacb15
Lordian Ayles <gkai70263@gmail.com>
2026-06-22 23:30:13 -0400

    stupid: disable diagnostic

--- lua/autorun/server/ja_arc9_jam.lua
+++ lua/autorun/server/ja_arc9_jam.lua
@@ -1,3 +1,4 @@
+---@diagnostic disable: undefined-field
 hook.Add("KeyPress", "JaVox ARC9 Jam Feature", function(ply, key)
     if key == IN_ATTACK then
         local activeWeapon = ply:GetActiveWeapon()
