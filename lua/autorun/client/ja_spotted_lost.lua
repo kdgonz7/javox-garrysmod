ac331b8a891ff5924a3bb70d9a120734a9947412
Lordian Ayles <gkai70263@gmail.com>
2026-06-23 18:04:55 -0400

    convar: add convars to the clientside loss module

--- lua/autorun/client/ja_spotted_lost.lua
+++ lua/autorun/client/ja_spotted_lost.lua
@@ -1,5 +1,3 @@
-local PlayWindSound = CreateClientConVar("javox_spotted_lost_wind_sound", "1", true, false)
-
 local SpottedHUDState = {
     isGoingUp = false,
     animationTextState = 1,
@@ -8,6 +6,15 @@ local SpottedHUDState = {
     isSpotted = false,
     windPlayed = false,
     lastSeenPosition = Vector(0, 0, 0)
+}
+
+local SpottedHUDSettings = {
+    AnimationDuration = CreateClientConVar("javox_spotted_lost_animation_duration", "5", true, false),
+    EnableTooltipText = CreateClientConVar("javox_spotted_lost_enable_tooltip_text", "1", true, false),
+    TooltipTextDuration = CreateClientConVar("javox_spotted_lost_tooltip_text_duration", "5", true, false),
+    PlayWindSound = CreateClientConVar("javox_spotted_lost_wind_sound", "1", true, false),
+    ShowText = CreateClientConVar("javox_spotted_lost_show_text", "1", true, false),
+    ShowPings = CreateClientConVar("javox_spotted_lost_show_pings", "1", true, false)
 }
 
 net.Receive("JaVox_EntLost", function()
@@ -19,7 +26,7 @@ net.Receive("JaVox_EntLost", function()
     SpottedHUDState.isGoingUp = true
     SpottedHUDState.isSpotted = false
 
-    if PlayWindSound:GetBool() then
+    if SpottedHUDSettings.PlayWindSound:GetBool() then
         if not SpottedHUDState.windPlayed then
             surface.PlaySound("ambient/wind/windgust.wav")
             SpottedHUDState.windPlayed = true
@@ -28,6 +35,7 @@ net.Receive("JaVox_EntLost", function()
 end)
 
 net.Receive("JaVox_EntSpotted", function()
+    if not SpottedHUDSettings.ShowText:GetBool() then return end
     local ent = net.ReadEntity()
     if not IsValid(ent) then return print("ent not valid") end
 
@@ -37,12 +45,13 @@ net.Receive("JaVox_EntSpotted", function()
     SpottedHUDState.isSpotted = true
     SpottedHUDState.windPlayed = false
 
-    print(ent)
     -- TODO: send vector directly from server instead of using ent:GetPos()
     SpottedHUDState.lastSeenPosition = ent:GetPos()
 end)
 
 hook.Add("HUDPaint", "JaVox_SpottedHUD", function()
+    if not SpottedHUDSettings.ShowText:GetBool() then return end
+
     local ply = LocalPlayer()
     if ! ply:IsValid() or ! ply:Alive() then
         -- reset states
@@ -52,7 +61,8 @@ hook.Add("HUDPaint", "JaVox_SpottedHUD", function()
         SpottedHUDState.lastSeenPosition = Vector(0, 0, 0)
         return
     end
-    if CurTime() - SpottedHUDState.lastLostTime > 5 then
+
+    if CurTime() - SpottedHUDState.lastLostTime > SpottedHUDSettings.AnimationDuration:GetFloat() then
         SpottedHUDState.isGoingUp = false
         SpottedHUDState.windPlayed = false
     end
@@ -71,6 +81,7 @@ hook.Add("HUDPaint", "JaVox_SpottedHUD", function()
     else
         SpottedHUDState.tooltipTextState = Lerp(0.02, SpottedHUDState.tooltipTextState, 0)
     end
+
     if SpottedHUDState.animationTextState > 0.001 then
         draw.SimpleText("Entity " .. (SpottedHUDState.isSpotted and "Spotted" or "Lost"), "ShareTech", ScrW() * 0.5,
             ScrH() * 0.7 - SpottedHUDState.animationTextState * 10,
@@ -90,17 +101,20 @@ end)
 
 -- render a visual indicator for the last seen position
 hook.Add("PostDrawTranslucentRenderables", "JaVox_SpottedIndicator", function()
+    if not SpottedHUDSettings.ShowPings:GetBool() then return end
+
     if ! SpottedHUDState.lastSeenPosition then return end
     if SpottedHUDState.animationTextState > 0.001 then
         local pos = SpottedHUDState.lastSeenPosition + Vector(0, 0, 45) -- float it up slightly
         local plyAng = LocalPlayer():EyeAngles()
-        local ang = Angle(0, plyAng.y - 90, 90)
+        plyAng:RotateAroundAxis(plyAng:Up(), -90)
+        plyAng:RotateAroundAxis(plyAng:Forward(), 90)
 
         local uiText = "Last Seen Position"
         local uiColor = SpottedHUDState.isSpotted and Color(255, 140, 0, 255 * SpottedHUDState.animationTextState) or
-        Color(255, 0, 0, 255 * SpottedHUDState.animationTextState)
+            Color(255, 0, 0, 255 * SpottedHUDState.animationTextState)
 
-        cam.Start3D2D(pos, ang, 0.2)
+        cam.Start3D2D(pos, plyAng, 0.2)
 
         cam.IgnoreZ(true)
 
