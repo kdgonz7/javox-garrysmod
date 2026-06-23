88f6a8f5493a9d7f61166ba8d4187c45951c93f2
Lordian Ayles <gkai70263@gmail.com>
2026-06-23 00:13:01 -0400

    test NEW fs builder method

---
+++ lua/javox_pack/ja_test.lua
@@ -0,0 +1,7 @@
+JaVox.FSBuilder:BuildFromFilesystem({
+    name = "test_module",
+    path = "fs",
+    author = "My Name",
+    description = "A test sound module.",
+    tags = { "test", "sounds" }
+})
