becea035cc25c2a5e9fd3d9701da3d6c70f36067
Lordian Ayles <gkai70263@gmail.com>
2026-06-23 18:04:43 -0400

    fix: server error on load

--- lua/autorun/!ja_fonts.lua
+++ lua/autorun/!ja_fonts.lua
@@ -1,4 +1,7 @@
-resource.AddFile("resource/fonts/sharetech.ttf")
+if SERVER then
+    resource.AddFile("resource/fonts/sharetech.ttf")
+    return
+end
 
 surface.CreateFont("ShareTech", {
     font = "Share Tech Mono",
