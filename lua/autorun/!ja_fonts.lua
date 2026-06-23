273896736a8fc5b79a68b00c44c1494f32859d18
Lordian Ayles <gkai70263@gmail.com>
2026-06-23 17:30:12 -0400

    font: ShareTech Mono

---
+++ lua/autorun/!ja_fonts.lua
@@ -0,0 +1,12 @@
+resource.AddFile("resource/fonts/sharetech.ttf")
+
+surface.CreateFont("ShareTech", {
+    font = "Share Tech Mono",
+    size = 24,
+    weight = 400
+})
+surface.CreateFont("ShareTechSmaller", {
+    font = "Share Tech Mono",
+    size = 16,
+    weight = 400
+})
