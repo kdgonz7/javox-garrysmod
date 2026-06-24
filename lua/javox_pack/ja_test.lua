114d7341fe08e4353fa315391892a440397d3392
Lordian Ayles <gkai70263@gmail.com>
2026-06-24 18:25:43 -0400

    remove: test module

--- lua/javox_pack/ja_test.lua
+++
@@ -1,7 +0,0 @@
-JaVox.FSBuilder:BuildFromFilesystem({
-    name = "test_module",
-    path = "fs",
-    author = "My Name",
-    description = "A test sound module.",
-    tags = { "test", "sounds" }
-})
