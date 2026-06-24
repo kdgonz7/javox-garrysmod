d5100e83514d6ebe147b19ea8c0a753cddc26acf
Lordian Ayles <gkai70263@gmail.com>
2026-06-24 16:36:38 -0400

    remove: old shot pain module

--- lua/autorun/server/ja_shot_pain.lua
+++
@@ -1,4 +0,0 @@
-hook.Add("EntityTakeDamage", "JaVOX Shot Pain Module", function(target, dmg)
-    -- TODO: check both entities, if recipient is player and they have preset, call their javox 'self.inflictedDamage.shot.rarm, larm, etc.', or specific ones
-    -- TODO: always have generic editions.
-end)
